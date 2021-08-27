3902a07073a81345cc764942cf62ca271dd6c496
최지우 <w2040@e-mirim.hs.kr>
2021-08-27 10:18:57 +0900

    package

--- module package/do.py
+++ module package/do.py
@@ -1,3 +1,26 @@
 import my_module as mm
 mm.지원이()
-print(mm.date)
+print(gp)
+
+import travel.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+from travel import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travel.gangreung import GangreungPackage
+gp = GangreungPackage()
+print(gp)
+
+from travel import ilsan
+ilsan.이지()
+
+from travel.ilsan import 이지
+이지()
+
+from travel import *
+ilsan.이지()
+gp = gangreung.GangreungPackage()
+print(gp)
