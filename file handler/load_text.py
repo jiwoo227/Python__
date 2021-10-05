baacb63f661ad378725b8e9f7cc55f8c6ffb7ee9
최지우 <w2040@e-mirim.hs.kr>
2021-10-05 11:26:49 +0900

    file list

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -1,7 +1,22 @@
-f = open('text.txt', 'r', encoding='uft-8')
 
+print('한꺼번에 전체 읽기')
+f = open('text.txt', 'r', encoding='utf-8')
 data = f.read()
-
-f.close()
-
 print(data)
+
+print('한 줄씩 일기')
+
+with open('text.txt', 'r', encoding='utf-8') as f:
+    while True:
+        line = f.readline()  #line: 'hello\n'
+        if line == '':   # 파일에서 익어올 내용 없으면 끝
+            break
+        print(line.rstrip()) #print('hello')
+
+
+print('한꺼번에 전체 읽어서, 한줄씩 리스트')
+with open('text.txt', 'r', encoding='utf-8')as f:
+    lines = f.readlines()
+    print(lines)
+for line in lines:
+    print(line)
