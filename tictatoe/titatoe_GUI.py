fbba0dd8bd7d84182d334d8dfb8a0f040cf30d9b
최지우 <w2040@e-mirim.hs.kr>
2021-11-09 12:20:06 +0900

    tictatoe GUI coordinate

--- tictatoe/titatoe_GUI.py
+++ tictatoe/titatoe_GUI.py
@@ -20,15 +20,27 @@ class TictatoeGUI:
         self.images['X'] = tkinter.PhotoImage(file='X.gif')
         self.images['X'] = tkinter.PhotoImage(file='O.gif')
 
-        self.canvas.bind('<Button-1>', self.click_handler) #click_handler 뒤에 ()가 없다는건 지금 실행하는게 아님 ()가 있으면 지금 실행하는것
+        self.canvas.bind('<Button-1>', self.click_handler)
+        #click_handler 뒤에 ()가 없다는건 지금 실행하는게 아님 ()가 있으면 지금 실행하는것
 
         self.root.mainloop()
 
     def click_handler(self, event):
-        print('click')
+        #print(event.x, event.y) click 위치 설정
+        #input event.x,evetn.y -> row,col
+        row,col = self.coordinate_to_position(event.x, event.y)
+        #set row,col
+        self.game_engine.set(row,col)
+        #show board
+        self.game_engine.show_board()
+        #승자가 있거나 무승부일 때 게임오버, 결과 출력하기
+        #change turn
+
 
     def draw_board(self):
         pass
 
+    def coordinate_to_position(self,x, y):
+       pass
 if __name__ == '__main__':
     ttt_GUI = TictatoeGUI()
